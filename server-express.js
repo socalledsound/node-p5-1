6a53d63d28be8eeeabaf348c5c1dc5e0f299bceb
socalledsound <lrs6000@gmail.com>
2017-10-08 13:23:17 -0700

    just putting these up here for class

---
+++ server-express.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+// app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (request, response) => {
+    //response.send('<h1>hello whatever, I am ok</h1>');
+    response.sendfile('/index.html')
+})
+
+
+
+app.listen(port, ()=> {
+	console.log(`server is running on port ${port}`);
+});
